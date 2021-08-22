74192e3a3cb24fb9604cffa9c0573a5668ec401d
afeezdev <afeezdev@gmail.com>
2021-08-22 12:48:05 +0100

    Completed the UI  design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import './App.css';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import Messenger from "./pages/messenger/Messenger";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+// import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  // const { user } = useContext(AuthContext);
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home /> 
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/messenger">
+          <Messenger />
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
